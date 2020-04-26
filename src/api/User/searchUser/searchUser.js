b893ff3c0551dec5a9376abaa56056f209a06729
cmlee <bal.dongdongdong@gmail.com>
2020-04-26 14:35:36 +0900

    search

---
+++ src/api/User/searchUser/searchUser.js
@@ -0,0 +1,12 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+  Query: {
+    searchUser: (_, args) =>
+      prisma.users({
+        where: {
+          OR: [{ username_contains: args.term }, { name_contains: args.term }],
+        },
+      }),
+  },
+};
